9f9e43cbbb9116aeb5b4dc2923ba730c224688b0
Erik Gilling <konkers@google.com>
2018-12-03 13:15:36 -0800

    Day 2 part 2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -46,6 +46,50 @@ fn checksum(ids: &Vec<String>) -> i64 {
     twos * threes
 }
 
+fn find_common(a: &str, b: &str) -> Option<String> {
+    if a.len() != b.len() {
+        return None;
+    }
+
+    let mut deviations = 0;
+    let mut common = String::new();
+    for i in a.chars().zip(b.chars()) {
+        if i.0 == i.1 {
+            common.push(i.0);
+        } else {
+            deviations += 1;
+            if deviations > 1 {
+                return None;
+            }
+        }
+    }
+
+    if deviations == 1 {
+        Some(common)
+    } else {
+        None
+    }
+}
+
+fn find_common_in_list(ids: &Vec<String>) -> Option<String> {
+    // We're assuming there are only two matching box IDs as inferred
+    // by the question "What letters are common between the two correct
+    // box IDs?"
+
+    // We're using an O(n^2) algorithm.  There is probably something fancy
+    // we can do with hashing/caching.  Since n == 250 in the input, let's
+    // stay simple.
+    for i in 0..(ids.len() - 1) {
+        for j in (i + 1)..ids.len() {
+            if let Some(s) = find_common(&ids[i], &ids[j]) {
+                return Some(s);
+            }
+        }
+    }
+
+    None
+}
+
 fn read<R: Read>(io: R) -> Result<Vec<String>, Error> {
     let br = BufReader::new(io);
     br.lines().collect()
@@ -53,7 +97,8 @@ fn read<R: Read>(io: R) -> Result<Vec<String>, Error> {
 
 fn main() -> Result<(), Error> {
     let lines = read(File::open("input.txt")?)?;
-    println!("Pt 1 answer: {} ", checksum(&lines));
+    println!("Pt 1 answer: {}", checksum(&lines));
+    println!("Pt 2 answer: {}", find_common_in_list(&lines).unwrap());
 
     Ok(())
 }
@@ -73,14 +118,27 @@ mod test {
         assert_eq!((false, true), analyze_id("ababab"));
 
         let strings = [
-            "abcdef",
-            "bababc",
-            "abbcde",
-            "abcccd",
-            "aabcdd",
-            "abcdee",
-            "ababab",
-        ].iter().map(|s| s.to_string()).collect();
+            "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab",
+        ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
         assert_eq!(12, checksum(&strings));
+    }
+
+    #[test]
+    fn part2_example_test() {
+        assert_eq!(None, find_common("a", "ab"));
+        assert_eq!(None, find_common("abcde", "axcye"));
+        assert_eq!(Some("fgij".to_string()), find_common("fghij", "fguij"));
+
+        let strings = [
+            "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz",
+        ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        assert_eq!(Some("fgij".to_string()), find_common_in_list(&strings));
     }
 }
