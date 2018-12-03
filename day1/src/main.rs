25f5c7696c0556bd9e63c38a0bd647799cafd974
Erik Gilling <konkers@google.com>
2018-12-03 11:29:54 -0800

    Day1 pt2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
 
@@ -6,6 +7,24 @@ fn calc_shift(start: i64, shifts: &[i64]) -> i64 {
 
     for shift in shifts {
         val += shift;
+    }
+
+    val
+}
+
+fn find_dup_freq(start: i64, shifts: &[i64]) -> i64 {
+    let mut val = start;
+    let mut seen = HashSet::new();
+
+    seen.insert(val);
+
+    'outer: loop {
+        for shift in shifts {
+            val += shift;
+            if seen.insert(val) == false {
+                break 'outer;
+            }
+        }
     }
 
     val
@@ -20,7 +39,8 @@ fn read<R: Read>(io: R) -> Result<Vec<i64>, Error> {
 
 fn main() -> Result<(), Error> {
     let shifts = read(File::open("input.txt")?)?;
-    println!("The answer: {}", calc_shift(0, &shifts));
+    println!("Pt 1 answer: {}", calc_shift(0, &shifts));
+    println!("Pt 2 answer: {}", find_dup_freq(0, &shifts));
     Ok(())
 }
 
@@ -33,5 +53,13 @@ mod test {
         assert_eq!(3, calc_shift(0, &[1, 1, 1]));
         assert_eq!(0, calc_shift(0, &[1, 1, -2]));
         assert_eq!(-6, calc_shift(0, &[-1, -2, -3]));
+    }
+
+    #[test]
+    fn part2_example_test() {
+        assert_eq!(0, find_dup_freq(0, &[1, -1]));
+        assert_eq!(10, find_dup_freq(0, &[3, 3, 4, -2, -4]));
+        assert_eq!(5, find_dup_freq(0, &[-6, 3, 8, 5, -6]));
+        assert_eq!(14, find_dup_freq(0, &[7, 7, -2, -7, -4]));
     }
 }
